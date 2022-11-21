fca5e664a9bea2d4f6e154596e4d9d63fb747033
MaykalDroshev-Hull <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2022-11-21 00:18:43 +0000

    Some Backend is done with comments

--- XMLpad/MainWindow.xaml.cs
+++ XMLpad/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Printing;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -17,6 +18,8 @@ using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.Folding;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System.IO;
+using System.Diagnostics;
 
 namespace XMLpad
 {
@@ -25,21 +28,249 @@ namespace XMLpad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string mFilename;
+        Microsoft.Win32.OpenFileDialog mDlgOpen = new Microsoft.Win32.OpenFileDialog();
+        Microsoft.Win32.SaveFileDialog mDlgSave = new Microsoft.Win32.SaveFileDialog();
+        PrintDialog mDlgPrint;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
+            Welcome_Window welcome_Window = new Welcome_Window();
+            if (welcome_Window.ShowDialog() == false)
+                Close();
+
             InitializeComponent();
+            GenerateInitializationText();
             textEditor.Focus();
-
         }
 
-        public void textEditor_TextChanged(object sender, KeyEventArgs e)
+        /// <summary>
+        /// Generates the initialization text.
+        /// </summary>
+        private void GenerateInitializationText()
         {
-            LineCharacterPosition.Content = $"Line: {textEditor.TextArea.Caret.Line} Column: {textEditor.TextArea.Caret.Column}";
+            try
+            {
+                FileStream fileStream = new FileStream("C:/temp/tempfile.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fileStream);
+                textEditor.Text = reader.ReadToEnd();
+                reader.Close();
+            }catch(IOException)
+            {
+                textEditor.Text = "Open a file or start writing a new one";
+            }
+
+            UpdateStatusBar(pAppend: false);
+            // TODO: check if a file is saved
+            // Then load the file
+            // Else create NewFile.xml and opn it here
         }
 
-        private void textEditor_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        /// <summary>
+        /// Handles the TextChanged event of the textEditor control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyEventArgs"/> instance containing the event data.</param>
+        public void textEditor_TextChanged(object sender, KeyEventArgs e) => UpdateStatusBar(pAppend: false);
+
+        /// <summary>
+        /// Handles the PreviewMouseDoubleClick event of the textEditor control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseButtonEventArgs"/> instance containing the event data.</param>
+        private void textEditor_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e) => UpdateStatusBar(pAppend: false);
+
+        /// <summary>
+        /// Handles the NewFile event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_NewFile(object sender, RoutedEventArgs e) => CheckIfCurrentFileIsSaved(textEditor.Text);
+
+        /// <summary>
+        /// Checks if current file is saved.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        private void CheckIfCurrentFileIsSaved(string text)
         {
-            LineCharacterPosition.Content = $"Line: {textEditor.TextArea.Caret.Line} Column: {textEditor.TextArea.Caret.Column}";
+            if(true/* TODO: text != textFile.text*/)
+            {
+                SaveCurrentFile(text);
+            }
+        }
+
+        /// <summary>
+        /// Saves the current file.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        private void SaveCurrentFile(/*mFilename*/ string text)
+        {
+            StreamWriter writer = new StreamWriter(mFilename);
+            writer.Write(textEditor.Text);
+            writer.Flush();
+            writer.Close();
+            UpdateStatusBar( pAppend: true, $" Wrote {textEditor.Text.Length} chars in { mFilename}");
+        }
+
+        /// <summary>
+        /// Updates the status bar.
+        /// </summary>
+        /// <param name="pAppend">if set to <c>true</c> [p append].</param>
+        /// <param name="pValue">The p value.</param>
+        private void UpdateStatusBar(bool pAppend, string pValue = "")
+        {
+            if (pAppend)
+                LineCharacterPosition.Content = $"{DateTime.Now.ToShortDateString()}  {DateTime.Now.ToShortTimeString()} : {pValue}";
+            else
+                LineCharacterPosition.Content = $"Line: {textEditor.TextArea.Caret.Line} Column: {textEditor.TextArea.Caret.Column} Chars: {textEditor.Text.ToCharArray().Length} ";
+        }
+
+        /// <summary>
+        /// Handles the OpenFile event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_OpenFile(object sender, RoutedEventArgs e)
+        {
+            if (mDlgOpen.ShowDialog() == true)
+            {
+                StreamReader reader = new StreamReader(mDlgOpen.FileName);
+                mFilename = mDlgOpen.FileName;
+                textEditor.Text = reader.ReadToEnd();
+                reader.Close();
+                UpdateStatusBar(pAppend: true, "Read " + mDlgOpen.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Handles the OpenContainingFolder event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_OpenContainingFolder(object sender, RoutedEventArgs e) => Process.Start("explorer.exe", "/select," + mFilename);
+
+        /// <summary>
+        /// Handles the Save event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_Save(object sender, RoutedEventArgs e) => SaveFile();
+
+        /// <summary>
+        /// Handles the SaveAs event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_SaveAs(object sender, RoutedEventArgs e) => SaveDialogFile(pCopy: false);
+
+        /// <summary>
+        /// Saves the dialog file.
+        /// </summary>
+        /// <param name="pCopy">if set to <c>true</c> [p copy].</param>
+        private void SaveDialogFile(bool pCopy)
+        {
+            mDlgSave.FileName += pCopy ? "- Copy" : "";
+            if (mDlgSave.ShowDialog() == true)
+            {
+                SaveFile();
+            }
+        }
+
+        /// <summary>
+        /// Saves the file.
+        /// </summary>
+        private void SaveFile()
+        {
+            FileStream fileStream = new FileStream(mDlgSave.FileName, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.Write(textEditor.Text);
+            writer.Flush();
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Handles the SaveACopyAs event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_SaveACopyAs(object sender, RoutedEventArgs e) => SaveDialogFile(pCopy: true);
+
+        /// <summary>
+        /// Handles the Print event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_Print(object sender, RoutedEventArgs e)
+        {
+            // Initialise a PrintDialog
+            mDlgPrint = new PrintDialog();
+
+            mDlgPrint.ShowDialog();
+        }
+
+        /// <summary>
+        /// Handles the Clear event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_Clear(object sender, RoutedEventArgs e)
+        {
+            ResetDlgs();
+            textEditor.Text = "";
+        }
+
+        /// <summary>
+        /// Handles the Exit event of the MainMenu_File control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainMenu_File_Exit(object sender, RoutedEventArgs e)
+        {
+            SaveTempFile();
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Saves the temporary file.
+        /// </summary>
+        private void SaveTempFile()
+        {
+            FileStream fileStream;
+            try
+            {
+                fileStream = new FileStream("C:/temp/tempfile.txt", FileMode.Truncate, FileAccess.Write);
+            }catch(FileNotFoundException)
+            {
+                fileStream = new FileStream("C:/temp/tempfile.txt", FileMode.Create, FileAccess.Write);
+            }
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.Write(textEditor.Text);
+            writer.Flush();
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Resets the dialogs.
+        /// </summary>
+        private void ResetDlgs()
+        {
+            mDlgOpen.FileName = "";
+            mDlgSave.FileName = "";
+            UpdateStatusBar(pAppend: true, "Ready ");
+        }
+
+        /// <summary>
+        /// Handles the Closing event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.CancelEventArgs"/> instance containing the event data.</param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveTempFile();
         }
     }
 }
---
+++ XMLpad/Welcome_Window.xaml.cs
@@ -0,0 +1,130 @@
+﻿using ICSharpCode.AvalonEdit;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Linq;
+
+namespace XMLpad
+{
+    /// <summary>
+    /// Interaction logic for Welcome_Window.xaml
+    /// </summary>
+    public partial class Welcome_Window : Window
+    {
+        /// <summary>
+        /// The timer
+        /// </summary>
+        DispatcherTimer timer = new DispatcherTimer();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Welcome_Window"/> class.
+        /// </summary>
+        public Welcome_Window()
+        {
+
+            InitializeComponent();
+            media.Source = new Uri(Environment.CurrentDirectory + @"\Loading_Screen.gif");
+            FillRecentOpenDocument();
+            Loading();
+
+        }
+
+        /// <summary>
+        /// Fills the recent open document List Box.
+        /// </summary>
+        private void FillRecentOpenDocument()
+        {
+            try
+            {
+                FileStream fileStream = new FileStream("C:/temp/XMLpadRecentFiles.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fileStream);
+                while (reader.Peek() >= 0)
+                {
+                    recentFilesListBox.Items.Add(reader.ReadLine());
+                }
+                reader.Close();
+            }
+            catch (IOException) {
+                recentFilesListBox.Items.Add("Create a new file");
+
+            }
+        }
+
+        /// <summary>
+        /// Handles the MediaEnded event of the MediaElement control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            media.Position = new TimeSpan(0, 0, 1);
+            media.Play();
+        }
+
+        /// <summary>
+        /// Handles the tick event of the timer control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void timer_tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            media.Visibility = Visibility.Hidden;
+            welcomeScreenControlsCanvas.Visibility = Visibility.Visible;
+
+        }
+
+        /// <summary>
+        /// Handles the Click event of the CloseButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        /// <summary>
+        /// Loadings this instance.
+        /// </summary>
+        void Loading()
+        {
+            timer.Tick += timer_tick;
+            timer.Interval = new TimeSpan(0, 0, 4);
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Invoked when an unhandled <see cref="E:System.Windows.UIElement.MouseLeftButtonDown" /> routed event is raised on this element. Implement this method to add class handling for this event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.Windows.Input.MouseButtonEventArgs" /> that contains the event data. The event data reports that the left mouse button was pressed.</param>
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the createNewButon control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void createNewButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
