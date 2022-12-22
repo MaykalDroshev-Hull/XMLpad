cdbd3845d8dbfa67b4f0f571d8d5e8ba234c1bf7
MaykalDroshev-Hull <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2022-12-22 14:14:33 +0000

    Added Skeleton files for methods and added a full screen mode

--- XMLpad/MainWindow.xaml.cs
+++ XMLpad/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Automation;
 
 namespace XMLpad
 {
@@ -29,6 +30,7 @@ namespace XMLpad
     public partial class MainWindow : Window
     {
         private string mFilename;
+        private static bool isFullScreen = false;
         Microsoft.Win32.OpenFileDialog mDlgOpen = new Microsoft.Win32.OpenFileDialog();
         Microsoft.Win32.SaveFileDialog mDlgSave = new Microsoft.Win32.SaveFileDialog();
         PrintDialog mDlgPrint;
@@ -59,7 +61,8 @@ namespace XMLpad
                 StreamReader reader = new StreamReader(fileStream);
                 textEditor.Text = reader.ReadToEnd();
                 reader.Close();
-            }catch(IOException)
+            }
+            catch (IOException)
             {
                 textEditor.Text = "Open a file or start writing a new one";
             }
@@ -97,7 +100,7 @@ namespace XMLpad
         /// <param name="text">The text.</param>
         private void CheckIfCurrentFileIsSaved(string text)
         {
-            if(true/* TODO: text != textFile.text*/)
+            if (true/* TODO: text != textFile.text*/)
             {
                 SaveCurrentFile(text);
             }
@@ -113,7 +116,7 @@ namespace XMLpad
             writer.Write(textEditor.Text);
             writer.Flush();
             writer.Close();
-            UpdateStatusBar( pAppend: true, $" Wrote {textEditor.Text.Length} chars in { mFilename}");
+            UpdateStatusBar(pAppend: true, $" Wrote {textEditor.Text.Length} chars in {mFilename}");
         }
 
         /// <summary>
@@ -244,7 +247,8 @@ namespace XMLpad
             {
                 fileStream = new FileStream("C:/temp/tempfile.txt", FileMode.Truncate, FileAccess.Write);
 
-            }catch(FileNotFoundException)
+            }
+            catch (FileNotFoundException)
             {
                 fileStream = new FileStream("C:/temp/tempfile.txt", FileMode.Create, FileAccess.Write);
             }
@@ -274,41 +278,180 @@ namespace XMLpad
             SaveTempFile();
         }
 
-        private void MenuItem_Undo(object sender, RoutedEventArgs e) => textEditor.Undo();
+        private void MainMenu_Undo(object sender, RoutedEventArgs e) => textEditor.Undo();
 
-        private void MenuItem_Redo(object sender, RoutedEventArgs e) => textEditor.Redo();
+        private void MainMenu_Redo(object sender, RoutedEventArgs e) => textEditor.Redo();
 
-        private void MenuItem_Cut(object sender, RoutedEventArgs e) => textEditor.Cut();
+        private void MainMenu_Cut(object sender, RoutedEventArgs e) => textEditor.Cut();
 
-        private void MenuItem_Copy(object sender, RoutedEventArgs e) => textEditor.Copy();
+        private void MainMenu_Copy(object sender, RoutedEventArgs e) => textEditor.Copy();
 
-        private void MenuItem_DuplicateLine(object sender, RoutedEventArgs e) => textEditor.DuplicateLine();//TODO Copy the files and make them editable
-        private void MenuItem_Paste(object sender, RoutedEventArgs e) => textEditor.Paste();
-        private void MenuItem_Delete(object sender, RoutedEventArgs e) => textEditor.Delete();
-        private void MenuItem_SelectAll(object sender, RoutedEventArgs e) => textEditor.SelectAll();
-        private void MenuItem_Insert_DateTimeShort(object sender, RoutedEventArgs e)
+        private void MainMenu_DuplicateLine(object sender, RoutedEventArgs e)
+        {
+            //textEditor.DuplicateLine();//TODO Copy the files and make them editable
+        }
+
+        private void MainMenu_Paste(object sender, RoutedEventArgs e) => textEditor.Paste();
+        private void MainMenu_Delete(object sender, RoutedEventArgs e) => textEditor.Delete();
+        private void MainMenu_SelectAll(object sender, RoutedEventArgs e) => textEditor.SelectAll();
+        private void MainMenu_Insert_DateTimeShort(object sender, RoutedEventArgs e)
         {
             textEditor.SelectedText += DateTime.Now.ToShortDateString();
         }
 
-        private void MenuItem_Insert_DateTimeLong(object sender, RoutedEventArgs e)
+        private void MainMenu_Insert_DateTimeLong(object sender, RoutedEventArgs e)
         {
             textEditor.SelectedText += DateTime.Now.ToLongDateString();
         }
 
-        private void MenuItem_Insert_DateTimeCustomized(object sender, RoutedEventArgs e)
+        private void MainMenu_Insert_DateTimeCustomized(object sender, RoutedEventArgs e)
         {
-            // todo insert the loaded custom date time format if created otherwise call MenuItem_Insert_CustomizeFormat after a dialog that it is not set
+            // todo insert the loaded custom date time format if created otherwise call MainMenu_Insert_CustomizeFormat after a dialog that it is not set
         }
 
-        private void MenuItem_Insert_CustomizeFormat(object sender, RoutedEventArgs e)
+        private void MainMenu_Insert_CustomizeFormat(object sender, RoutedEventArgs e)
         {
             // Todo: Create a window for customizing date time format
         }
-        private void MainMenu_IncreaseIndent(object sender , RoutedEventArgs e)
+        private void MainMenu_IncreaseIndent(object sender, RoutedEventArgs e)
         {
             //todo find the string between the cursor and the previous carriage return \r\n and insert  tab or 4 spacesdepending on prefernece
         }
 
+        private void MainMenu_Copy_CurrentFilePath(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Copy_CurrentFileName(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_DecreaseIndent(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region ConvertCase
+        private void MainMenu_ConvertCase_AllUppercase(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_ConvertCase_AllLowercase(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_ConvertCase_InvertCase(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_ConvertCase_RandomCase(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+        #region LineOperations
+        private void MainMenu_LineOperations_DuplicateLine(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_RemoveDuplicateLines(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_RemoveConsecutiveDuplicateLines(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_MoveUpCurrentLine(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_MoveDownCurrentLine(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_RemoveEmptyLines(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_InsertBlankLineAboveCurrent(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_LineOperations_InsertBlankLineBelowCurrent(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+
+        #region Comment/Uncomment
+        private void MainMenu_Comment(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Uncomment(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+
+        #region BlankOperations
+        private void MainMenu_Edit_BlankOperations_TrimTrailingSpace(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_TrimLeadingSpace(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_TrimTrailingAndLeadingSpace(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_TabToSpace(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_SpaceToTabLeading(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_SpaceToTabTrailing(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Edit_BlankOperations_SpaceToTabAll(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+        private void MainMenu_Edit_SetAsReadOnly(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region View
+        private void MainMenu_View_ToggleFullScreen(object sender, RoutedEventArgs e)
+        {
+            if (!isFullScreen)
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+                MenuItem_FullScreen.Header = "Toggle Normal Screen Mode";
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.WindowState = WindowState.Normal;
+                MenuItem_FullScreen.Header = "Toggle Full Screen Mode";
+            }
+            isFullScreen = !isFullScreen;
+        }
+            #region ShowSymbols
+        private void MainMenu_View_ShowSymbol_ShowSpacesAndTabs(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_View_ShowSymbol_ShowNewLines(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_View_ShowSymbol_ShowAllCharacters(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+        #endregion
+        #region Zoom
+        private void MainMenu_View_ZoomIn(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_View_ZoomOut(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_View_ZoomDefault(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+        #region Settings/Help
+        private void MainMenu_Settings_EnvironmentPreferences(object sender, RoutedEventArgs e)
+        {
+        }
+        private void MainMenu_Help_ViewManual(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
     }
 }
