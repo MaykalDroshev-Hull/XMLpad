4af0e28ebfa565175267d2fd04341b3a8b8654ba
MaykalDroshev-Hull <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2023-02-28 23:40:35 +0000

    Added icons, need to fix the async methods

---
+++ XMLpad/Windows/LoadingPopUp.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XMLpad
+{
+    /// <summary>
+    /// Interaction logic for LoadingPopUp.xaml
+    /// </summary>
+    public partial class LoadingPopUp : Window
+    {
+        private readonly TaskCompletionSource<bool> _hiddenTcs = new TaskCompletionSource<bool>();
+
+        public LoadingPopUp()
+        {
+            InitializeComponent();
+            Closed += (_, _) => _hiddenTcs.TrySetResult(true);
+        }
+
+        public async Task HideAsync()
+        {
+            Hide();
+            await _hiddenTcs.Task;
+        }
+    }
+}
