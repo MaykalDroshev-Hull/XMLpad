d6ede594f93597eddbec4dae6eb99c81ba56582b
MaykalDroshev-Hull <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2023-03-19 09:36:08 +0000

    Added MIT license comment on all c# files and included the usings in the namespace on some files

--- XMLpad/Windows/CompareFiles.xaml.cs
+++ XMLpad/Windows/CompareFiles.xaml.cs
@@ -1,43 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.IO;
-using static ICSharpCode.AvalonEdit.Rendering.TextViewWeakEventManager;
-using Azure;
-using DiffMatchPatch;
-using Microsoft.SqlServer.Management.Sdk.Differencing;
-using Newtonsoft.Json.Linq;
-using static System.Net.WebRequestMethods;
-using ICSharpCode.AvalonEdit.Document;
-using ICSharpCode.AvalonEdit.Highlighting;
-using ICSharpCode.AvalonEdit.Rendering;
-using System.Text.RegularExpressions;
-using ICSharpCode.AvalonEdit;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using Application = System.Windows.Application;
-using Window = System.Windows.Window;
-using File = System.IO.File;
-using MessageBox = System.Windows.MessageBox;
-using ICSharpCode.AvalonEdit;
-using ICSharpCode.AvalonEdit.Rendering;
-using Microsoft.SqlServer.Management.Sdk.Sfc;
-using Microsoft.SqlServer.Management.Smo;
-using ScintillaNET;
-using static ScintillaNET.Style;
-
+﻿// Copyright (c) 2023 Maykal Droshev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sub license, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 namespace XMLpad
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using DiffMatchPatch;
+    using Window = System.Windows.Window;
+    using File = System.IO.File;
+    using MessageBox = System.Windows.MessageBox;
+
     /// <summary>
     /// Interaction logic for CompareFiles.xaml
     /// </summary>
--- XMLpad/Windows/LoadingPopUp.xaml.cs
+++ XMLpad/Windows/LoadingPopUp.xaml.cs
@@ -1,45 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿// Copyright (c) 2023 Maykal Droshev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sub license, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 namespace XMLpad
 {
+    using System;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+
+
     /// <summary>
     /// Interaction logic for LoadingPopUp.xaml
     /// </summary>
     public partial class LoadingPopUp : Window
     {
+        /// <summary>
+        /// The hidden TCS
+        /// </summary>
         private readonly TaskCompletionSource<bool> _hiddenTcs = new TaskCompletionSource<bool>();
+
+        /// <summary>
+        /// The shown TCS
+        /// </summary>
         private readonly TaskCompletionSource<bool> _shownTcs = new TaskCompletionSource<bool>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadingPopUp"/> class.
+        /// </summary>
         public LoadingPopUp()
         {
             InitializeComponent();
             Closed += (_, _) => _hiddenTcs.TrySetResult(true);
         }
 
+        /// <summary>
+        /// Shows the asynchronous.
+        /// </summary>
         public async Task ShowAsync()
         {
             Show();
             await _shownTcs.Task;
         }
 
+        /// <summary>
+        /// Hides the asynchronous.
+        /// </summary>
         public async Task HideAsync()
         {
             Hide();
             await _hiddenTcs.Task;
         }
 
+        /// <summary>
+        /// Handles the ContentRendered event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             _shownTcs.TrySetResult(true);
